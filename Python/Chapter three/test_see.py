a1d98bf7269f102321619c010e28acf98eb01b2d
Ejika Collins Ojochenemi <160156263+happyboy24@users.noreply.github.com>
2025-09-29 12:26:32 +0100

    Update test_see.py

--- Python/Chapter three/test_see.py
+++ Python/Chapter three/test_see.py
@@ -2,7 +2,7 @@ for i in range(2):
     value = int(input('Enter an integer (-1 to break): '))
     print('You entered:', value)
     if value == -1:
-        break # Breaks the loop early
+        break 
 else:
    
     print('The loop terminated without executing the break')
