75fe421bbba2effa18a74cf7d00b5bf8511f1faf
happyboy24 <cejika134@gmail.com>
2025-10-18 18:03:25 +0100

    full commit

---
+++ Java script/PalindromeStr.java
@@ -0,0 +1,21 @@
+public class PalindromeString{
+	public static void main(String [] args){
+	
+String name = "Ebunoluwa";
+String store = " ";
+
+String letters = "";
+
+		for(int count = name.length() - 1 ; count >= 0; count--){
+			letters += String.valueOf(name.charAt(count));
+		}
+
+		
+		if(name.equals (letters)){
+			System.out.print("It is a palindrome");
+		}
+		else{
+			System.out.print("It is not a palindrome");
+		}
+	}
+}
---
+++ Java script/positionvowel.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class PositionVowel{
+	public static void main(String [] args){
+	
+	String name = {"Ebunoluwa"}
+	String [] position = new String [name.length];
+	String vowel = "aeiou";
+
+	for(int count = 0; count < name.length; count++){
+		position [count] = name[count]
